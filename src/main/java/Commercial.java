ec2ff33a8e087597cb64fa4aa3d12924b55a88e7
Administrateur <Administrateur@M2I-HERM-10.formation.m2i>
2017-09-04 15:08:45 +0200

    Complétude du commercial

--- src/main/java/Commercial.java
+++ src/main/java/Commercial.java
@@ -1,4 +1,55 @@
 
 public class Commercial {
-	private String matircule;
+	private String matricule;
+
+	public Commercial(String matricule) {
+		super();
+		this.matricule = matricule;
+	}
+
+	/**
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+
+	/**
+	 * @param matricule the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((matricule == null) ? 0 : matricule.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Commercial other = (Commercial) obj;
+		if (matricule == null) {
+			if (other.matricule != null)
+				return false;
+		} else if (!matricule.equals(other.matricule))
+			return false;
+		return true;
+	}
+	
 }
